2a1b0020bcb50d0ab516da7a68eb722775bdbb30
Ángel Ortega <codenode.ortega@gmail.com>
2024-05-24 11:41:27 -0600

    Refactor package structure for controllers and models

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,8 +7,6 @@ module org.blackjack.blackjack {
     exports org.blackjack.blackjack;
     exports org.blackjack.blackjack.controller;
     opens org.blackjack.blackjack.controller to javafx.fxml;
-    exports org.blackjack.blackjack.controller.game;
-    opens org.blackjack.blackjack.controller.game to javafx.fxml;
     exports org.blackjack.blackjack.models;
     opens org.blackjack.blackjack.models to javafx.fxml;
 }
