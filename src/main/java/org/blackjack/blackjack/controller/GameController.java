8897020d2ec90b2213a6a312f9e04c7fce719ede
Ángel Ortega <codenode.ortega@gmail.com>
2024-05-24 12:25:47 -0600

    chore: Update GameController to display deck in ListView

--- src/main/java/org/blackjack/blackjack/controller/GameController.java
+++ src/main/java/org/blackjack/blackjack/controller/GameController.java
@@ -1,4 +1,21 @@
 package org.blackjack.blackjack.controller;
+import org.blackjack.blackjack.models.Deck;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
 
 public class GameController {
+    @FXML
+    private ListView<String> deckView;
+
+    private Deck deck;
+
+    public void initialize() {
+        deck = new Deck();
+        updateDeckView();
+    }
+
+    private  void updateDeckView(){
+        deckView.getItems().clear();
+        deck.getDeck().forEach(card -> deckView.getItems().add(card.toString()));
+    }
 }
