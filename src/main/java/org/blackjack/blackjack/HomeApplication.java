ce630a986b62d967774239cbb1202b4d7d463363
Ángel Ortega <codenode.ortega@gmail.com>
2024-06-10 18:08:13 -0600

    Refactor HomeApplication and GameController classes for improved code structure and readability

--- src/main/java/org/blackjack/blackjack/HomeApplication.java
+++ src/main/java/org/blackjack/blackjack/HomeApplication.java
@@ -4,20 +4,20 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 
 import java.io.IOException;
 
 public class HomeApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HomeApplication.class.getResource("home-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 620, 540);
-        stage.setTitle("Blackjack");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("Home-view.fxml"));
+        primaryStage.setTitle("Blackjack Game");
+        primaryStage.setScene(new Scene(root, 800, 600));
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
--- src/main/java/org/blackjack/blackjack/controller/GameController.java
+++ src/main/java/org/blackjack/blackjack/controller/GameController.java
@@ -2,6 +2,14 @@ package org.blackjack.blackjack.controller;
 import org.blackjack.blackjack.models.Deck;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
 
 public class GameController {
     @FXML
@@ -11,11 +19,83 @@ public class GameController {
 
     public void initialize() {
         deck = new Deck();
-        updateDeckView();
+        start(new Stage());
+        new Stage();
     }
 
-    private  void updateDeckView(){
-        deckView.getItems().clear();
-        deck.getDeck().forEach(card -> deckView.getItems().add(card.toString()));
+    public void start(Stage primaryStage) {
+        //Pane root = new Pane();
+        //Scene scene = new Scene(root, 800, 600);
+
+        // Crear las áreas de las cartas del dealer
+        Rectangle dealerCard1 = new Rectangle(300, 50, 50, 70);
+        dealerCard1.setFill(Color.RED);
+        Rectangle dealerCard2 = new Rectangle(360, 50, 50, 70);
+        dealerCard2.setFill(Color.GRAY);
+
+        // Crear las áreas de las cartas del jugador
+        Rectangle playerCard1 = new Rectangle(300, 400, 50, 70);
+        playerCard1.setFill(Color.BLUE);
+        Rectangle playerCard2 = new Rectangle(360, 400, 50, 70);
+        playerCard2.setFill(Color.RED);
+
+        // Crear el área de apuestas y dinero
+        Label moneyLabel = new Label("Money");
+        moneyLabel.setLayoutX(650);
+        moneyLabel.setLayoutY(20);
+
+        Label betLabel = new Label("Apuesta");
+        betLabel.setLayoutX(720);
+        betLabel.setLayoutY(20);
+
+        // Crear botones (simulando las acciones del juego)
+        Button hitButton = new Button("Hit");
+        hitButton.setLayoutX(650);
+        hitButton.setLayoutY(500);
+
+        Button standButton = new Button("Stand");
+        standButton.setLayoutX(700);
+        standButton.setLayoutY(500);
+
+        Button doubleButton = new Button("Double");
+        doubleButton.setLayoutX(760);
+        doubleButton.setLayoutY(500);
+
+        // Agregar elementos al root
+        //root.getChildren().addAll(dealerCard1, dealerCard2, playerCard1, playerCard2, moneyLabel, betLabel, hitButton, standButton, doubleButton);
+
+        //primaryStage.setTitle("Blackjack Game");
+        //primaryStage.setScene(scene);
+        //primaryStage.show();
     }
+
+    @FXML
+    private Rectangle dealerCard1;
+
+    @FXML
+    private Rectangle dealerCard2;
+
+    @FXML
+    private Rectangle playerCard1;
+
+    @FXML
+    private Rectangle playerCard2;
+
+    @FXML
+    private Label moneyLabel;
+
+    @FXML
+    private Label betLabel;
+
+    @FXML
+    private Button hitButton;
+
+    @FXML
+    private Button standButton;
+
+    @FXML
+    private Button doubleButton;
 }
+
+
+
